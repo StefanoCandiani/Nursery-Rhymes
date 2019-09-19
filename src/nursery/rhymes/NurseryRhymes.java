91ed5c881c5e085de8c1b695b9dfcb54d6fdc705
22500 <22500@MININT-58O3Q68.columbushs.com>
2019-09-19 08:50:02 -0400

    Done

--- src/nursery/rhymes/NurseryRhymes.java
+++ src/nursery/rhymes/NurseryRhymes.java
@@ -15,14 +15,33 @@ public class NurseryRhymes {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        System.out.println("Old MacDonald had a farm");
         
         farm("cow", "moo");
-        farm("duck", "quack");        
-        // TODO: add another animal to the farm here
-        hickory_dickory(1);
-        hickory_dickory(2);
-        // TODO: make the clock strike three here
+        farm("duck", "quack");
+        farm("turkey", "glu");
+        System.out.println("");
+        System.out.println("");
+        monkeys(10);
+        System.out.println("");
+        monkeys(9);
+        System.out.println("");
+        monkeys(8);
+        System.out.println("");
         
+        hickory_dickory(1);
+        System.out.println("");
+        hickory_dickory(2);
+        System.out.println("");
+        hickory_dickory(3);
+        System.out.println("");
+        
+        milk(99);
+        System.out.println("");
+        milk(98);
+        System.out.println("");
+        milk(97);
+        System.out.println("");
         // TODO: call your new methods here ( you must write them first! ) 
         
         
@@ -31,17 +50,48 @@ public class NurseryRhymes {
     
     
     public static void farm(String animal, String sound){
-        // TODO: write your code here
+        System.out.print("Old MacDonald had a farm \n"
+                        +"e-i-e-i-o \n"
+                        +"And on the farm, he had a "+animal+"\n"
+                        +"e-i-e-i-o \n"
+                        +"With a "+sound+" "+sound+" here \n"
+                        +"And a "+sound+" "+sound+" there \n"
+                        +"Here a "+sound+", there a "+sound+"\n"
+                        +"Everywhere a "+sound+" "+sound+" \n"
+                        +"Old MacDonald had a farm \n"
+                        +"e-i-e-i-o");
+        System.out.println("");
     }
     
     public static void monkeys(int number){
-        // TODO: write your code here
+        System.out.print(number+" little monkeys jumping on the bed \n"
+                               +"One fell off and bumped his head \n"
+                               +"Mama called the doctor, and the doctor said: \n"
+                               +"\"No more jumping on the bed!\"");
+        System.out.println("");
     }
     
     public static void hickory_dickory(int time){
-        // TODO: write your code here
+        System.out.println(time);
     }
     
-    // TODO add your new methods here
+    public static void milk(int bottles){
+        System.out.print(bottles +" bottles of milk on the wall \n"
+                        +bottles +" bottles of milk \n"
+                        +         "Take one down and pass it around \n");
+                        bottles -= 1;
+                        
+        System.out.print (bottles+ " bottles of milk on the wall");
+    }
+    
+    public static void hokey_pokey(String booty){
+        System.out.print("You put your "+booty+" in \n"
+                        +"You put your "+booty+" out \n"
+                        +"You put your "+booty+" in \n"
+                        +"And you shake it all about \n"
+                        +"You do the Hokey Pokey \n"
+                        +"And you turn yourself about \n"
+                        +"That's what it's all about!");
+    }
     
 }
